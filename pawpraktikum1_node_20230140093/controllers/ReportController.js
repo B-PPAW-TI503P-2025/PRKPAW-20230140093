6f0ff91e0fda3491c020e9edf916f6ab5558a875
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-10-24 10:03:42 +0700

    Praktikum koneksi database mysql untuk presensi

--- pawpraktikum1_node_20230140093/controllers/ReportController.js
+++ pawpraktikum1_node_20230140093/controllers/ReportController.js
@@ -1,8 +1,15 @@
-const presensiRecords = require("../data/PresensiData");
-exports.getDailyReport = (req, res) => {
-  console.log("Controller: Mengambil data laporan harian dari array...");
-  res.json({
-    reportDate: new Date().toLocaleDateString(),
-    data: presensiRecords,
-  });
+const { Presensi } = require("../models");
+
+exports.getDailyReport = async (req, res) => {
+  try {
+    console.log("Controller: Mengambil data laporan harian dari database...");
+    const data = await Presensi.findAll(); // ✅ ambil data dari tabel Presensi
+    res.json({
+      reportDate: new Date().toLocaleDateString(),
+      data, // hasil query aman dikirim ke client
+    });
+  } catch (error) {
+    console.error("Error saat mengambil laporan harian:", error);
+    res.status(500).json({ message: error.message });
+  }
 };
---
+++ tugas/tugas4.md
@@ -0,0 +1,10 @@
+1.Tampilan End Point CheckIn:
+   ![Check In](./screenshots4/CHECKIN.png)
+2.Tampilan Table Database CheckIn:
+   ![Table Database Check In](./screenshots4/TABLECHECKIN.png)
+3.Tampilan End Point ChekOut:
+   ![Check Out](./screenshots4/CHECKOUT.png)
+4.Tampilan Table Database CheckOut:
+   ![Table Database CheckOut](./screenshots4/TABLECHECKOUT.png)
+5.Tampilan End Point ReportsDaily:
+   ![Reports Daily](./screenshots4/REPORTSDAILY.png)
